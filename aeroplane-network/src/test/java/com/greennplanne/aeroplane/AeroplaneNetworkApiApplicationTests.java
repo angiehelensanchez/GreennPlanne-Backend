c82c10c128ee3a7d692a5b7f1a7a88111325b766
angiehelensanchez <angiehelensanchez@gmail.com>
2025-02-19 16:09:23 +0100

    build: initial commit

---
+++ aeroplane-network/src/test/java/com/greennplanne/aeroplane/AeroplaneNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greennplanne.aeroplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroplaneNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
