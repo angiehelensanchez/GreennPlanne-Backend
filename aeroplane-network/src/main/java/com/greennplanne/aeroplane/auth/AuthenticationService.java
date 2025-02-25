87d42eebf05abe358c66c06af09f442c07a9fc84
angiehelensanchez <angiehelensanchez@gmail.com>
2025-02-25 13:07:03 +0100

    feat: add AuthenticationController, AuthenticationService and Register Request

---
+++ aeroplane-network/src/main/java/com/greennplanne/aeroplane/auth/AuthenticationService.java
@@ -0,0 +1,11 @@
+package com.greennplanne.aeroplane.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+
+    public void register(RegistrationRequest request) {
+    }
+}
