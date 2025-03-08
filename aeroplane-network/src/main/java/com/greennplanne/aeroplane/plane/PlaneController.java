25b3428c9fe04f5ff6ace4a64bfe2aa9326139cb
angiehelensanchez <angiehelensanchez@gmail.com>
2025-03-08 13:43:10 +0100

    feat: add getAllPlanes

--- aeroplane-network/src/main/java/com/greennplanne/aeroplane/plane/PlaneController.java
+++ aeroplane-network/src/main/java/com/greennplanne/aeroplane/plane/PlaneController.java
@@ -1,5 +1,6 @@
 package com.greennplanne.aeroplane.plane;
 
+import com.greennplanne.aeroplane.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +27,13 @@ public class PlaneController {
             @PathVariable("plane-id") Integer planeId
     ) {
         return ResponseEntity.ok(planeService.findById(planeId));
+    }
+
+    @GetMapping("/planes")
+    public ResponseEntity<PageResponse<PlaneResponse>> getAllPlanes(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size
+    ){
+        return ResponseEntity.ok(planeService.findAllPLanes(page, size));
     }
 }
